47b7ac211fff2820bffdd93f5b3e3486de2ba982
Diego Brocanelli <diegod2@msn.com>
2017-01-05 19:17:44 -0200

    Criação de estrutura da library para continuidade das issues e implementação dos recursos e testes.

---
+++ src/Date/Validation/BR.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace DataTools\Date\Validation;
+
+/**
+ * @author Developer Space <developer.space.br@gmail.com>
+ */
+class BR
+{
+	/**
+	 * Validate date
+	 * 
+	 * @param string $date format en
+	 * @return boolean
+	 */
+	public static function isValid($date)
+	{
+		
+	}
+
+	/**
+	 * Verify if is empty
+	 * 
+	 * @param string $date format en
+	 * @return boolean
+	 */
+	public static function isEmptry($date)
+	{
+		
+	}
+}
